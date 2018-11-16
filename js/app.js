531b50f927740fa185fba7b9074ba1076a5f72d3
Natalia <natalia132@wp.pl>
2018-11-16 19:30:31 +0100

    classes names review

--- js/app.js
+++ js/app.js
@@ -1,7 +1,50 @@
 
+const endpoint = 'https://restcountries.eu/rest/v2/all';
+const countries = [];
+const names = [];
+
+fetch(endpoint)
+    .then(resp => resp.json())
+    .then(data => countries.push(...data))
+    .then(countries.map(country => names.push(...country.name)))
+    .then(console.log(names));
+
+const array = countries.map(country => country).join('');
+console.log(array);
+
+// console.log(countries);
+
+
+// function loadArray(){
+// cities.map(place => console.log(place)).join('');
+// }
+//
+//
+// window.addEventListener('load', loadArray)
+
+    // fetch('https://restcountries.eu/rest/v2/all')
+    // .then(function(resp) {
+    //   return resp.json()
+    //   })
+    //   .then(function(data) {
+    //     countries.push(...data)
+    //     })
+    //   .catch(function(error) {
+    //     console.log(error);
+    //   });
+
+
+
+//  const arr = [1, 'sdf', 2333]
+ // console.log(countries);
+
+// arr.forEach(aa => console.log(aa))
+
 
 google.charts.load('current', {
   'packages':['geochart'],
+  // Note: you will need to get a mapsApiKey for your project.
+  // See: https://developers.google.com/chart/interactive/docs/basic_load_libs#load-settings
   'mapsApiKey': 'AIzaSyBDrW_7FAqvg-1jfkGLcMupHWIMbUv6vWU'
 });
 google.charts.setOnLoadCallback(drawRegionsMap);
@@ -18,6 +61,7 @@ function drawRegionsMap() {
   ]);
 
   var options = {
+    backgroundColor: '#81d4fa',
     legend: 'none',
   };
 
